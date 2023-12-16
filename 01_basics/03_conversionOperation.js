ad90fad18b676ab7d0192ca2d36d51407d1b971d
Debidatta Mohanta <debidattamohanta1@gmail.com>
2023-12-16 22:59:47 +0530

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Piyush";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//  true => 1; false => 0
+
+
+let isLoggedIn = "piyush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
